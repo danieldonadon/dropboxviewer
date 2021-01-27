730b947465f6dd8948d399b8aa0fd4ff10cb942d
Daniel Donadon <donadon@gmail.com>
2021-01-26 22:53:29 -0300

    Changes to files.

--- dropboxviewer/settings.py
+++ dropboxviewer/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'viewer.apps.ViewerConfig',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Custom settings
+DROPBOX_KEY_FILE = 'dropbox.key'
